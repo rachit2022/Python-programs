9707f7c3644b10210747a2b3e4f9717b534025bc
Rachit Gujpta <102291472+rachit2022@users.noreply.github.com>
2023-03-05 18:59:10 +0530

    Add files via upload

---
+++ arthmetic.py
@@ -0,0 +1,8 @@
+a = 10
+b = 5
+print("The sum of a and b is:", a+b)
+print("The subtraction of a and b is:", a-b)
+print("The multiplication of a and b is:", a*b)
+print("The float division of a and b is:", a/b)
+print("The exponent of a**b is:", a**b)
+print("The division of a and b is:", a//b)
